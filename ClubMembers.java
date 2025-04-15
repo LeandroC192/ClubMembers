406db26d21b67410475ae38810ea14d1f5fca5c7
LeandroC192 <Leandroc16@nycstudents.net>
2025-04-15 19:25:51 +0000

    Completed Code

--- ClubMembers.java
+++ ClubMembers.java
@@ -1,22 +1,59 @@
+import java.lang.reflect.Member;
+import java.util.ArrayList;
 public class ClubMembers {
     private ArrayList<MemberInfo> memberList;
-
+    ArrayList<MemberInfo> alumni = new ArrayList<MemberInfo>();
+    public ClubMembers()
+    {
+        memberList = new ArrayList<MemberInfo>();
+    }
 
     /**
      * Adds new club members to memberList, as described in part (a).
      * Precondition: names is a non-empty array.
      */
-    public void addMembers(String[] names, int gradYear) {
-        /* to be implemented in part (a) */ }
-
+    public void addMembers(String[] names, int gradYear)
+    {
+        for(String s: names)
+        {
+            memberList.add(new MemberInfo(s, gradYear, true));
+        }
+        /* to be implemented in part (a) */ 
+    }
 
     /**
      * Removes members who have graduated and returns a list of members who have
      * graduated
      * and are in good standing, as described in part (b).
      */
-    public ArrayList<MemberInfo> removeMembers(int year) {
-        /* to be implemented in part (b) */ }
+    public ArrayList<MemberInfo> removeMembers(int year) 
+    {
+        for(int i = 0; i < memberList.size(); i++)
+        {
+            MemberInfo member = memberList.get(i);
+            if(member.getGradYear() <= year)
+            {
+                if(member.inGoodStanding())
+                {
+                    alumni.add(member);
+                }
+                memberList.remove(i);
+                i--;
+            }
+        }
+        return alumni;
+        /* to be implemented in part (b) */ 
+    }
     // There may be instance variables, constructors, and methods that are not
     // shown.
+    public ArrayList<MemberInfo> getMemberList() 
+    {
+        return memberList;
+        /* to be implemented in part (b) */ 
+    }
+
+    public void setMemberList(ArrayList<MemberInfo> members)
+    {
+        memberList = members;
+    }
 }
